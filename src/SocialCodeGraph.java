2655eb89499b221679d8080c4f328b175b094dee
Hirad Roshandel <Hiradroshandel@yahoo.com>
2014-01-26 18:08:09 -0800

    l

--- src/SocialCodeGraph.java
+++ src/SocialCodeGraph.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.egit.github.core.SearchRepository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
 
 public class SocialCodeGraph {
 
@@ -13,4 +19,17 @@ public class SocialCodeGraph {
 	{
 		
 	}
+	
+	public List<SearchRepository> searchRepositories(String query)
+	{	List<SearchRepository> repoList = null;
+		RepositoryService repo=new RepositoryService();
+		try {			
+			repoList=repo.searchRepositories(query);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return repoList;
+	}
 }
+ 
