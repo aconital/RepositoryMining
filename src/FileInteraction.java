e3dd15c5e58af5b3307fbe2be4a7ba7f47ce8d91
Hirad Roshandel <Hiradroshandel@yahoo.com>
2014-03-14 00:20:53 -0700

    added file integaration

--- src/FileInteraction.java
+++ src/FileInteraction.java
@@ -1,10 +1,12 @@
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 
 
 public class FileInteraction implements Edge {
-
-	private String data;
+	//change this to calender
+	private HashMap<String,String> interaction;
 	private Resource rsc;
 	private Person user;
 	
@@ -12,41 +14,62 @@ public class FileInteraction implements Edge {
 	{
 		this.rsc=rsc;
 		this.user=user;
-		
+		interaction=new HashMap<String, String>();
 	}
 	public FileInteraction(Resource rsc,Person user,String data)
-	{	this.data=data;
+	{	
 		this.rsc=rsc;
 		this.user=user;
 		
 	}
-	public String getData()
+
+	public void addInteraction(String sha,String data)
 	{
-		return data;
+		interaction.put(sha, data);
 	}
-	public void setData(String data)
+	public String getData(Calendar sha)
 	{
-		this.data=data;
+		return interaction.get(sha);
 	}
 
 
-
-
-
-	@Override
-	public boolean hasNode(Node n1,Node n2) {
-		// TODO Auto-generated method stub
+	public Node getHead()
+	{return rsc;}
+	public Node getTail()
+	{return user;}
+	public boolean hasData(String data)
+	{
+		if(interaction.containsValue(data))
+			return true;
 		return false;
 	}
 	@Override
-	public String getData(Calendar date) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean hasNode(Node n1,Node n2) {
+		Resource temp1= (Resource)n1;
+		Person temp2= (Person)n2;
+		if((temp1.equals(rsc) && temp2.equals(user)))
+			return true;
+		return false;
 	}
 	@Override
-	public String DateToString(Calendar date) {
-		// TODO Auto-generated method stub
-		return null;
+	public String DateToString(Calendar sha) {
+		int year=sha.get(Calendar.YEAR);;
+		int month=sha.get(Calendar.MONTH);;
+		int day=sha.get(Calendar.DAY_OF_MONTH);
+		String stringsha= Integer.toString(year)+"/"+Integer.toString(month)+"/"+Integer.toString(day);
+
+		return stringsha;
+	}
+	
+	public void printDetails()
+	{	System.out.println("**********************");
+		System.out.println("File: "+rsc.getFilename()+" User: "+user.getName());
+		Iterator it= interaction.keySet().iterator();
+		while(it.hasNext())
+		{	String key=(String) it.next();
+			System.out.println("sha: "+key+" Data: "+interaction.get(key));
+		}
+		System.out.println("**********************");
 	}
 
 }
--- src/SocialCodeGraph.java
+++ src/SocialCodeGraph.java
@@ -17,14 +17,23 @@ import java.util.Set;
 
 import org.apache.commons.collections15.Transformer;
 import org.eclipse.egit.github.core.Comment;
+import org.eclipse.egit.github.core.Commit;
+import org.eclipse.egit.github.core.CommitFile;
 import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryCommit;
 import org.eclipse.egit.github.core.RepositoryIssue;
 import org.eclipse.egit.github.core.SearchIssue;
 import org.eclipse.egit.github.core.SearchRepository;
 import org.eclipse.egit.github.core.User;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.event.IssueCommentPayload;
+import org.eclipse.egit.github.core.event.PullRequestPayload;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.service.GitHubService;
 import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.PullRequestService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.eclipse.egit.github.core.service.UserService;
 
@@ -48,16 +57,20 @@ import javax.swing.JFrame;
 public class SocialCodeGraph {
 	static GitHubClient client = new GitHubClient();
 	private static String oAuthKey="50ec05b630c6d418473809c9f8946f18df33dd7e";
+	//##########################
+	//no static -refactor later
+	//##########################
 	private static Set<UserInteraction> userInts;
 	private static Set<Person> users;
-	private static Person user1;
-	private static Person user2;
+	private static Set<FileInteraction> fileInts;
+	private static Set<Resource> resources;
+
 	private static  DirectedGraph<Node, Edge> graph =new DirectedSparseMultigraph<Node, Edge>();
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 
 		//Finding the right Repository (NEED REFACTOR)
 		client.setOAuth2Token(oAuthKey);
@@ -75,82 +88,97 @@ public class SocialCodeGraph {
 		int indexOfRepo= userInputScanner.nextInt();
 		SearchRepository selectedRepo= repoList.get(indexOfRepo);
 		System.out.println("You have selected "+selectedRepo.getName()+" Owner:"+selectedRepo.getOwner());
+		System.out.println("Please Wait! Processing...");
+
 
 		populateSystem(selectedRepo);
+
 		createGraph();
-		//Now that we have our Repo we populate the system
-	
+
+
 	}
 
 	public static void createGraph()
 	{		for(UserInteraction ui: userInts)
-			{	graph.addVertex(ui.getHead());
-				graph.addVertex(ui.getTail());
-				graph.addEdge(ui, ui.getHead(), ui.getTail());
-			}
-	
-	     Layout<Node, Edge> layout = new CircleLayout(graph);
-	    //   layout.setSize(new Dimension(500,500));
-
-	        BasicVisualizationServer<Node,Edge> vv = new BasicVisualizationServer<Node,Edge>(layout);
-	        vv.setPreferredSize(new Dimension(500,500));    
-	        vv.setGraphLayout(layout);
-	        vv.scaleToLayout(new LayoutScalingControl());
-	      //  vv.setAutoscrolls(true);
-	 
-	        vv.setBounds(0, 0, 500, 500);
-	        // Setup up a new vertex to paint transformer...
-	        Transformer<Node,Paint> vertexPaint = new Transformer<Node,Paint>() {
-	            public Paint transform(Node i) {
-	                return Color.GREEN;
-	            }
-	        };  
-	        // Set up a new stroke Transformer for the edges
-	        float dash[] = {10.0f};
-	        final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
-	             BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
-	        Transformer<Edge, Stroke> edgeStrokeTransformer = new Transformer<Edge, Stroke>() {
-	            public Stroke transform(Edge s) {
-	                return edgeStroke;
-	            }
-	        };
-	        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
-	        vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
-	     
-	        //make this clickable so user can choose the interaction
-	         vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller(){
-	        	@Override
-	        	 public String transform(Object e)
-	        	{
-	        		return ((Person) e).getName();
-	        	}
-	         });
-	        
-	        vv.getRenderer().getVertexLabelRenderer().setPosition(Position.E);        
-	   
-	        JFrame frame = new JFrame("Repository Interaction Graph");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.getContentPane().add(vv);
-	        frame.pack();
-	        frame.setVisible(true);  
+	{	graph.addVertex(ui.getHead());
+	graph.addVertex(ui.getTail());
+	graph.addEdge(ui, ui.getHead(), ui.getTail());
 	}
-	
-	
+	for(FileInteraction fi:fileInts)
+	{
+		graph.addVertex(fi.getHead());
+		graph.addVertex(fi.getTail());
+		graph.addEdge(fi, fi.getHead(), fi.getTail());
+	}
+	Layout<Node, Edge> layout = new CircleLayout(graph);
+	//   layout.setSize(new Dimension(500,500));
+
+	BasicVisualizationServer<Node,Edge> vv = new BasicVisualizationServer<Node,Edge>(layout);
+	vv.setPreferredSize(new Dimension(700,700));    
+	vv.setGraphLayout(layout);
+	vv.scaleToLayout(new LayoutScalingControl());
+	//  vv.setAutoscrolls(true);
+
+	vv.setBounds(0, 0, 700, 700);
+	// Setup up a new vertex to paint transformer...
+	Transformer<Node,Paint> vertexPaint = new Transformer<Node,Paint>() {
+		public Paint transform(Node i) {
+			if(i instanceof Person)
+				return Color.GREEN;
+			else
+				return Color.BLUE;
+		}
+	};  
+	// Set up a new stroke Transformer for the edges
+	float dash[] = {10.0f};
+	final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+			BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
+	Transformer<Edge, Stroke> edgeStrokeTransformer = new Transformer<Edge, Stroke>() {
+		public Stroke transform(Edge s) {
+			return edgeStroke;
+		}
+	};
+	vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+	vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
+
+	//make this clickable so user can choose the interaction
+	vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller(){
+		@Override
+		public String transform(Object e)
+		{	if(e instanceof Person)
+			return ((Person) e).getName();
+		else
+			return ((Resource) e).getFilename();
+		}
+	});
+
+	vv.getRenderer().getVertexLabelRenderer().setPosition(Position.E);        
+
+	JFrame frame = new JFrame("Repository Interaction Graph");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.getContentPane().add(vv);
+	frame.pack();
+	frame.setVisible(true);  
+	}
+
+
 	public static void populateSystem(SearchRepository repo)
 	{
 		IssueService issue=new IssueService(client);
 		List<Comment> comments;
 		userInts = new HashSet<UserInteraction>();
+		fileInts=new HashSet<FileInteraction>();
+		resources=new HashSet<Resource>();
 		users=new HashSet<Person>();
-			
+
 		Map<String,String> params =new HashMap<String,String>();
 		params.put("state", "open");
 
 		try { 
-		
+
 			for(Issue xissue: issue.getIssues(repo,params ))
 			{
-					
+
 				comments=issue.getComments(repo, xissue.getNumber());
 
 				//Creating an edge between Asignee and others
@@ -158,108 +186,195 @@ public class SocialCodeGraph {
 				//Creating an edge between Creator & commenter (Think when creator comments again?!)
 				createCommenterInteraction(xissue,comments);
 			}
-			
-			
+
+
+			//PullRequest Section
+
+			List<RepositoryCommit> repositoryCommitList;
+			Set<String> shaList =new HashSet<String>();
+			CommitService cs=new CommitService(client);
+			repositoryCommitList=cs.getCommits(repo);
+
+			for(RepositoryCommit rc:repositoryCommitList)	
+			{	
+				shaList.add(rc.getSha());			
+			}
+			for(String sha:shaList)
+			{
+				RepositoryCommit repCommit=cs.getCommit(repo, sha);
+				if(repCommit!=null){
+				List<CommitFile> commitFileList=repCommit.getFiles();
+				User user=repCommit.getCommitter();
+				String commiter="";	
+				if(user!=null){
+						commiter=user.getLogin();
+						String message=repCommit.getCommit().getMessage();
+						//API CURRENTLY DOESNT HAVE DATE
+						createFileInteraction(commiter,commitFileList,sha,message);
+					}
+				}
+
+			}
+			for(FileInteraction fi:fileInts)
+			{
+				fi.printDetails();
+			}
+			//######### CREATE UI#########
 			for(UserInteraction ui:userInts)
 			{
 				ui.printDetails();
 			}
-		
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+
+
+
+
+
+
 	}
-	
+	private static void createFileInteraction(String commiter,List<CommitFile> commitFileList,String sha,String message) 
+	{
+		Person user=userFinder(commiter);
+		Resource resource;
+
+		for(CommitFile cf:commitFileList)
+		{	
+		
+			resource=resourceFinder(cf.getFilename());
+			FileInteractionFinder(resource,user,sha,message);
+		}
+	}
+	private static void FileInteractionFinder(Resource rsc,Person user,String sha,String data)
+	{
+			if(rsc==null || user== null)
+				return;
+			
+			for(FileInteraction fi: fileInts)
+			{
+				if(fi.hasNode(rsc, user))
+				{
+					if(!fi.hasData(data))
+					{
+						fi.addInteraction(sha, data);
+						return;
+					}
+				}
+			}
+			FileInteraction fileInt=new FileInteraction(rsc, user);
+			fileInt.addInteraction(sha, data);
+			fileInts.add(fileInt);
+			return;
+
+	}
+	private static Resource resourceFinder(String name)
+	{	for(Resource rsc:resources)
+		{
+		if(rsc.getFilename().equals(name))
+			return rsc;
+		}
+		Resource r=new Resource(name);
+		resources.add(r);
+		return r;
+	}
+
 	private static void createCommenterInteraction(Issue xissue, List<Comment> comments) {
+		Person user1;
+		Person user2;
 		//between commenters
 		for(Comment c:comments)
 		{ 	user1=userFinder(c.getUser().getLogin());
-			for(Comment d:comments)
-			{	Calendar date=DateToCalendar(d.getCreatedAt());
-				user2=userFinder(d.getUser().getLogin());
-				interactionFinder(user1,user2, date, c.getBody());
-			}
-			
+		for(Comment d:comments)
+		{	Calendar date=DateToCalendar(d.getCreatedAt());
+		user2=userFinder(d.getUser().getLogin());
+		UserInteractionFinder(user1,user2, date, c.getBody());
+		}
+
 		}
 		//between Creator and commenters
 		for(Comment c:comments)
 		{ 	user1=userFinder(c.getUser().getLogin());
-			user2=userFinder(xissue.getUser().getLogin());
-			Calendar date=DateToCalendar(c.getCreatedAt());
-			interactionFinder(user1,user2, date, c.getBody());
-			
+		user2=userFinder(xissue.getUser().getLogin());
+		Calendar date=DateToCalendar(c.getCreatedAt());
+		UserInteractionFinder(user1,user2, date, c.getBody());
+
 		}
-		
+
 	}
 
 	private static void createAssigneeInteraction(Issue xissue,List<Comment> comments) {
+		Person user1;
+		Person user2;
 		if(xissue.getAssignee()== null)
 			return;
 		String interactionType="";
 		//assignee
 		user1=userFinder(xissue.getAssignee().getLogin());
-		
+
 		//between creator and assignee
-			user2=userFinder(xissue.getUser().getLogin());
-			Calendar date=DateToCalendar(xissue.getCreatedAt());
-			interactionFinder(user1, user2, date, xissue.getBody());
-		
-		
+		user2=userFinder(xissue.getUser().getLogin());
+		Calendar date=DateToCalendar(xissue.getCreatedAt());
+		UserInteractionFinder(user1, user2, date, xissue.getBody());
+
+
 		//between commenters and assignee
 		for(Comment c:comments)
 		{
 			{
 				user2=userFinder(c.getUser().getLogin());
-				 date=DateToCalendar(xissue.getCreatedAt());
+				date=DateToCalendar(xissue.getCreatedAt());
 				interactionType="Commenter-Assignee Interaction";
-				interactionFinder(user1, user2, date, interactionType);
-			
+				UserInteractionFinder(user1, user2, date, interactionType);
+
 			}
 		}
 	}
-	private static void interactionFinder(Person user1,Person user2,Calendar date,String data)
+	private static void UserInteractionFinder(Person user1,Person user2,Calendar date,String data)
 	{	if(user1.equals(user2))
 		return;
-	
-		for(UserInteraction ui:userInts)
-		{
-			if(ui.hasNode(user1, user2))
-			{	if(!ui.hasData(data))
-				ui.addInteraction(date, data);
-				return;
-			}
-		}
-		
-		UserInteraction userInt=new UserInteraction(user1, user2);
-		userInt.addInteraction(date,data);
-		userInts.add(userInt);
+
+	for(UserInteraction ui:userInts)
+	{
+		if(ui.hasNode(user1, user2))
+		{	if(!ui.hasData(data))
+			ui.addInteraction(date, data);
 		return;
+		}
+	}
+
+	UserInteraction userInt=new UserInteraction(user1, user2);
+	userInt.addInteraction(date,data);
+	userInts.add(userInt);
+	return;
 	}
 	private static Person userFinder(String name)
 	{	for(Person user:users)
-		{
-			if(user.getName().toString().equals(name))
-				return user;
-		}
-		Person p=userCreation(name);
-		users.add(p);
-		return p;
+	{
+		if(user.getName().toString().equals(name))
+			return user;
+	}
+	Person p=userCreation(name);
+	users.add(p);
+	return p;
 	}
 	private static Person userCreation(String name)	
 	{	
-	UserService u=new UserService(client);
-	User gitUser;
-	Person user = null;
-	try {
-		gitUser = u.getUser(name);
-		user=new Person(gitUser.getLogin(), gitUser.getEmail()) ;
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		UserService u=new UserService(client);
+		User gitUser;
+		Person user = null;
+		try {
+			gitUser = u.getUser(name);
+			user=new Person(gitUser.getLogin(), gitUser.getEmail()) ;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	return user;
+		return user;
 
 	}
 	private static Calendar DateToCalendar(Date date){ 
