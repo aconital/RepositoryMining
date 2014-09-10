8025b5dceec8259a6c5e43ce89ec1ca1d217b00d
Hirad Roshandel <p30lord.hirad@yahoo.com>
2014-09-09 21:26:35 -0700

    fix

--- src/SocialCodeGraph.java
+++ src/SocialCodeGraph.java
@@ -52,7 +52,7 @@ import javax.swing.JFrame;
  * who had interacted with each other [currently] through issues and commits and draws a graph
  * 
  * HOW TO: (1)Simply run the application and type in the name of the repository which you are 
- * looking for. The application searches within GitHub for all repository which similar name
+ * looking for [For example: massren]. The application searches within GitHub for all repository which have similar name
  * (2) Enter the index number of the repository which you are looking
  * (3) Once the graph is created, use mouse scroll to zoom in/out, User T and P buttons to toggle
  * between picking mode and camera mode. In picking mode you can select a node and move it around.
